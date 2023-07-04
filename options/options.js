8732842a706e37315bd132626a913e005e8615b6
Crowdbotics <team@crowdbotics.com>
2023-07-04 01:53:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "luigi-solutions-41890",
+  url: "https://luigi-solutions-41890.botics.co",
+  api: "https://luigi-solutions-41890.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
